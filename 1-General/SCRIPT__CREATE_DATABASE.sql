c990c563c55619b7653fb390243e9f4468b13440
heberGustavo <heber.gbarbosa@hotmail.com>
2024-06-18 07:34:30 -0300

    How to create a Database

---
+++ 1-General/SCRIPT__CREATE_DATABASE.sql
@@ -0,0 +1,11 @@
+-- NOTE: It's necessary to execute 1 item at a time!
+
+
+/*To create a new database*/
+CREATE DATABASE DB_BIBLIOTECA
+
+/*To access a database created*/
+USE DB_BIBLIOTECA
+
+/*To see the settings of the database created*/
+SP_HELPDB DB_BIBLIOTECA
